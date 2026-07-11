10a0c93d6b4153d4914ee1c5acb6059c64d08bbb
Dhriti Bishnoi <dhritibishnoi1@gmail.com>
2026-07-11 23:34:12 +0530

    Add files via upload

---
+++ 25ESKCA068/DAY 9/test.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "hi", "world";
+print "hi";
+print("hi"); */
+$age = 18;
+if($age>=18){
+    print("Adult");
+}else{
+    print("Teenager");
+}
+echo"<br>";
+
+//conditional
+
+($age>=18)? print("Adult") : print ("Teenager"); */
+
+
+?>
